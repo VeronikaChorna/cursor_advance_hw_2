290063ca86fa21382694ad94eb3ea0017bac1589
nika_chorna <veronika.chorna2000@gmail.com>
2022-12-13 16:12:03 +0200

    add loops

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ while (Number.isInteger(numberN) === false) {
     console.log(numberN);
 }
 
-let numberM = Number(prompt("Please enter the last number:"));
+let numberM = Number(prompt("Please enter the last number (the number must be greater than the previous one):"));
 while (Number.isInteger(numberM) === false) {
     numberM = Number(prompt("It is not a correct value, please enter an integer number:"));
     console.log(numberM);
@@ -13,28 +13,21 @@ while (Number.isInteger(numberM) === false) {
 let skipEvenNumbers = confirm("Should to skip even numbers?") ? true : false;
 console.log(skipEvenNumbers);
 
-for (let i = numberN; i <= numberM; i++) { 
-    console.log(i);
-  }
+if (skipEvenNumbers === true) {
+    for (let i = numberN; i <= numberM; i++) { 
+        if (i % 2 !== 0){
+           console.log(i);
+        } 
+    } 
+    } else {
+        for (let i = numberN; i <= numberM; i++)
+        console.log(i);
+    }
+    
 
 
-
-
-// if (Number.isInteger(numberN) === false) {
-//     numberN = Number(prompt("It is not a correct value, please enter an integer number:"))
-// } else {
-//     numberN = Number(prompt("It is not a correct value, please enter an integer number:"))
-// } 
-
-// const numberM = Number(prompt("Please enter last number:"));
-
-
-// const isNumberNInreger = Number.isInteger(numberN);
-// const isNumberMInreger = Number.isInteger(numberM);
-// console.log(isNumberNInreger);
-// console.log(isNumberMInreger);
-
-
-// if (Number.isInteger(numberM) === false) {
-//     Number(prompt("Number is not correct, please enter correct number:"))
-// };
+// for (let i = numberN; i <= numberM; i++) { 
+//     if (i % 2 !== 0){
+//        console.log(i);
+//     }
+//   }
