c7ab1754a647913613dd816fc1a032e3f401c835
nika_chorna <veronika.chorna2000@gmail.com>
2022-12-13 22:50:48 +0200

    1 version

--- script/script.js
+++ script/script.js
@@ -1,33 +1,34 @@
 let numberN = Number(prompt("Please enter the first number:"));
 while (Number.isInteger(numberN) === false) {
     numberN = Number(prompt("It is not a correct value, please enter an integer number:"));
-    console.log(numberN);
 }
 
 let numberM = Number(prompt("Please enter the last number (the number must be greater than the previous one):"));
 while (Number.isInteger(numberM) === false) {
     numberM = Number(prompt("It is not a correct value, please enter an integer number:"));
-    console.log(numberM);
 }
 
 let skipEvenNumbers = confirm("Should to skip even numbers?") ? true : false;
-console.log(skipEvenNumbers);
+console.log(`Should to skip even numbers: ${skipEvenNumbers}`);
 
-if (skipEvenNumbers === true) {
-    for (let i = numberN; i <= numberM; i++) { 
-        if (i % 2 !== 0){
-           console.log(i);
-        } 
-    } 
-    } else {
-        for (let i = numberN; i <= numberM; i++)
-        console.log(i);
-    }
+for (let i = numberN; i <= numberM; i++) {
     
+}
 
 
-// for (let i = numberN; i <= numberM; i++) { 
-//     if (i % 2 !== 0){
-//        console.log(i);
+// let sum = 0;
+// if (skipEvenNumbers) {
+//     for (let i = numberN; i <= numberM; i++) { 
+//         if (i % 2){
+//            console.log(i);
+//            sum += i;
+//         }
 //     }
-//   }
+// } else {
+//     for (let i = numberN; i <= numberM; i++) { 
+//             console.log(i);
+//             sum += i;
+//     }
+// }
+    
+// console.log(sum);
